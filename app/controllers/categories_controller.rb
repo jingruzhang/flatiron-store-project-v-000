16d08f0c1bbe431a078dfde10a14548c1246e0f4
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-08 02:30:35 +0000

    Done with the Category feature spec

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,17 @@
 class CategoriesController < ApplicationController
 
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  private
+
+  def category_params
+    params.required(:category).permit(:title)
+  end
+
+  def set_category
+    @category = Category.find_by(id: params[:id])
+  end
 end
