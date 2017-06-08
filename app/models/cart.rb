4201f77e9a8430975a0d41f6c4267a932c1e928f
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-08 02:12:18 +0000

    Done with the model tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,32 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def total
+    total = 0
+    line_items.each do |line_item|
+      total += line_item.item.price * line_item.quantity
+    end
+    total
+  end
+
+  def add_item(item_id)
+    line_item = line_items.find_by(item_id: item_id)
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = line_items.build(item_id: item_id)
+    end
+    line_item
+  end
+
+  def check_out
+    line_items.each do |line_item|
+      line_item.item.inventory -= line_item.quantity
+      line_item.item.save
+    end
+    update(status: "submitted")
+  end
 
 end
