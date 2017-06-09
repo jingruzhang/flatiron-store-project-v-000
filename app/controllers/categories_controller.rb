becf348067e34809be9b419016fb92ff138c5b6d
JingruZhang <zhang.jingru.93@gmail.com>
2017-06-09 13:48:06 -0500

    clean up unnecessary files and codes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,6 @@
 class CategoriesController < ApplicationController
 
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
-
   def show
-  end
-
-  private
-
-  def category_params
-    params.required(:category).permit(:title)
-  end
-
-  def set_category
     @category = Category.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'welcome/home'
 
   root 'store#index', as: 'store'
 
-  resources :items, only: [:show, :index]
+  resources :items, only: [:show]
   resources :categories, only: [:show, :index]
   resources :users, only: [:show]
   resources :carts, only: [:show]
   resources :line_items, only: [:create]
-  resources :orders, only: [:show]
-
   post 'carts/:id/checkout', to: 'carts#checkout', as: 'checkout'
 
 end
